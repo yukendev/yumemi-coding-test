fc3494a3b84fdde8d3c3850530881c03397338b0
yuken <yuken0906dev@gmail.com>
2022-12-10 00:40:28 +0900

    create Header

---
+++ src/components/atoms/HeaderTitle.tsx
@@ -0,0 +1,3 @@
+export const HeaderTitle = (): JSX.Element => {
+  return <h1>都道府県の人口推移</h1>;
+};
---
+++ src/components/organisms/Header.tsx
@@ -0,0 +1,9 @@
+import { HeaderTitle } from '../atoms/HeaderTitle';
+
+export const Header = (): JSX.Element => {
+  return (
+    <>
+      <HeaderTitle />
+    </>
+  );
+};
